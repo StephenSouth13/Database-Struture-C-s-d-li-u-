eca6665241689f03fe61c45bad5f60d565c2369c
Quách Thành Long <stephensouth1307@gmail.com>
2026-05-07 22:34:18 +0700

    Update Exam câu 1 và đáp án

--- Buoi_2/Bai_Tap/01_schema_and_seed.sql
+++ Buoi_2/Bai_Tap/01_schema_and_seed.sql
@@ -28,5 +28,36 @@ CREATE TABLE Runways(
  2.3) Bảng Flights
  */
  CREATE TABLE Flights(
-     
+     FlightID VARCHAR(20) PRIMARY KEY,
+     AirlineCode Varchar(10) NOT NULL,
+     RunwayID VARCHAR(10) NOT NULL,
+     FlightType VARCHAR(20) NOT NULL,
+     CHECK (FlightType IN ('Passenger','Cargo')),
+     ScheduleTime DATETIME NOT NULL,
+     ActualTime DATETIME NOT NULL,
+     Direction VARCHAR(15) NOT NULL,
+     CHECK (Direction IN ('Departure','Arrival')),
+     CONSTRAINT FK_Flight_Airlines
+                     FOREIGN KEY (AirlineCode)
+                     REFERENCES Airlines(AirlineCode),
+     CONSTRAINT FK_Flight_Runways
+                     FOREIGN KEY (RunwayID)
+                     REFERENCES Runways(RunwayID)
+
  )
+/*
+ 2.4) Bảng CargoShipments
+ */
+ CREATE TABLE CargoShipments (
+     CargoID INT IDENTITY PRIMARY KEY,
+     FlightID VARCHAR(20) NOT NULL ,
+     WeightKG DECIMAL(10,2) NOT NULL,
+     CargoCategory NVARCHAR(50) NOT NULL,
+     CHECK (CargoCategory IN ('Perishable', 'Electronics', 'General')),
+     CustomsStatus VARCHAR(30) NOT NULL,
+     CHECK (CustomsStatus IN ('Cleared' , 'Pending', 'Rejected')),
+     CONSTRAINT FK_Cargo_Flights
+                             FOREIGN KEY (FlightID)
+                             REFERENCES Flights(FlightID)
+ )
+
---
+++ Exam/lythuyet.md
@@ -0,0 +1,30 @@
+
+# Câu 1. Tìm khóa của lược đồ quan hệ
+
+Cho lược đồ quan hệ:
+
+```text
+R = {A, B, C, D, E}
+```
+
+Tập phụ thuộc hàm:
+
+```text
+F = {
+    AB → C
+    CD → E
+    B → CE
+}
+```
+
+Xét tập phụ thuộc hàm tối thiểu:
+
+```text
+G = {
+    CD → E
+    B → CE
+    A → C
+}
+```
+
+Hãy tìm khóa của quan hệ `R`.
